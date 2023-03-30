f93135818edeb6e2b81d5882ef2c1970c1222181
Carlos Daniel Ochoa <115586395+cochoa1607@users.noreply.github.com>
2023-03-30 11:35:17 -0600

    Add files via upload

--- js.js
+++ js.js
@@ -1,19 +1,28 @@
-const shareData = {
-    title: "Carlos Ochoa",
-    text: "Conoce a Carlos, Diseñador y Desarrollador Web",
-    url: "https://www.google.com",
-  };
-  
-  const btn = document.querySelector("button");
-  const resultPara = document.querySelector(".result");
-  
-  // Share must be triggered by "user activation"
-  btn.addEventListener("click", async () => {
-    try {
-      await navigator.share(shareData);
-      resultPara.textContent = "Contacto compartido exitosamente!";
-    } finally {
-      resultPara.textContent = "Contacto compartido exitosamente Dos!";
+const btnCompartir = document.getElementById('btnCompartir');
+
+    btnCompartir?.addEventListener('click', () => {
+      share();
+    });
+
+    async function share() {
+        try {
+            let shareData = {
+                title: "Title Share MDN.",
+                text: "Ejemplo para conpartir un link",
+                url: "https://developer.mozilla.org",
+            };
+
+            if (!navigator.canShare) {
+                alert("navigator.canShare() not supported.");
+            } else if (navigator.canShare(shareData)) {
+                await navigator.share({
+                    title: '@erick_calderon | Linktree',
+                    url: 'https://linktree.ee/erick_calderon'
+                });
+            } else {
+                alert("Specified data cannot be shared.");
+            }
+        } catch (error) {
+            console.log('Sharing failed!', error)
+        }
     }
-  });
-  
