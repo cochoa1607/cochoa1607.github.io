5188b388d62871fb21f4c0a04dfbb653ecde3a47
Carlos Daniel Ochoa <115586395+cochoa1607@users.noreply.github.com>
2023-03-30 11:19:30 -0600

    Initial commit

---
+++ js.js
@@ -0,0 +1,19 @@
+const shareData = {
+    title: "Carlos Ochoa",
+    text: "Conoce a Carlos, Diseñador y Desarrollador Web",
+    url: "https://www.google.com",
+  };
+  
+  const btn = document.querySelector("button");
+  const resultPara = document.querySelector(".result");
+  
+  // Share must be triggered by "user activation"
+  btn.addEventListener("click", async () => {
+    try {
+      await navigator.share(shareData);
+      resultPara.textContent = "Contacto compartido exitosamente!";
+    } catch (err) {
+      resultPara.textContent = `Error: ${err}`;
+    }
+  });
+  
