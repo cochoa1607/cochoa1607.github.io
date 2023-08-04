252f41af3af2e8cd9adaffd5ac4ec88105f0252c
Carlos Ochoa <115586395+cochoa1607@users.noreply.github.com>
2023-08-04 14:36:40 -0600

    Changes
    Cambios del día

--- dev/rod/js/sliderInicioServicios.js
+++ dev/rod/js/sliderInicioServicios.js
@@ -1,1 +1,39 @@
 
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.getElementsByClassName('slide');
+    const dots = document.getElementsByClassName('dot');
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 6000);
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    const slides = document.getElementsByClassName('slide');
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    } else if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+showSlides();
