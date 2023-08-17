29b1fc1389b06e464858e2548ba8b8e9a9ba05c1
Carlos Ochoa <115586395+cochoa1607@users.noreply.github.com>
2023-08-17 17:57:50 -0600

    r

--- dev/rod/js/collapseServicios.js
+++ dev/rod/js/collapseServicios.js
@@ -9,10 +9,43 @@ function masInfo() {
 
 
 /* Auditoria */
-document.querySelector(".auditoria_masInfo").addEventListener("click", masInfoAudit);
+document.querySelector(".revision_masInfo").addEventListener("click", masInfoRev);
 
-var contTextoAudit = document.querySelector(".contenedor-texto-audit");
+var contTextoRev = document.querySelector(".contenedor-texto-rev");
 
-function masInfoAudit() {
-    contTextoAudit.classList.toggle("active-contenedor-texto-audit");
+function masInfoRev() {
+    contTextoRev.classList.toggle("active-contenedor-texto-rev");
+}
+
+
+
+/* Revision */
+document.querySelector(".revision_masInfo").addEventListener("click", masInfoRev);
+
+var contTextoRev = document.querySelector(".contenedor-texto-rev");
+
+function masInfoRev() {
+    contTextoRev.classList.toggle("active-contenedor-texto-rev");
+}
+
+
+
+/* Levantamiento */
+document.querySelector(".levantamiento_masInfo").addEventListener("click", masInfoLevan);
+
+var contTextoLevan = document.querySelector(".contenedor-texto-levan");
+
+function masInfoLevan() {
+    contTextoLevan.classList.toggle("active-contenedor-texto-levan");
+}
+
+
+
+/* Analisis */
+document.querySelector(".analisis_masInfo").addEventListener("click", masInfoAnalis);
+
+var contTextoAnalis = document.querySelector(".contenedor-texto-analis");
+
+function masInfoAnalis() {
+    contTextoAnalis.classList.toggle("active-contenedor-texto-analis");
 }
--- dev/rod/js/sliderServicios.js
+++ dev/rod/js/sliderServicios.js
@@ -78,3 +78,137 @@ function changeSlideAudit(n) {
 }
 
 showSlidesAudit();
+
+
+
+
+/* Revision */
+
+let slideIndexRev = 0;
+
+function showSlidesRev() {
+    const slidesRev = document.getElementsByClassName('slideRev');
+
+    for (let i = 0; i < slidesRev.length; i++) {
+        slidesRev[i].style.display = 'none';
+    }
+
+    slideIndexRev++;
+
+    if (slideIndexRev > slidesRev.length) {
+        slideIndexRev = 1;
+    }
+
+    slidesRev[slideIndexRev - 1].style.display = 'block';
+
+    setTimeout(showSlidesRev, 6000);
+}
+
+function changeSlideRev(n) {
+    slideIndexRev += n;
+    const slidesRev = document.getElementsByClassName('slideRev');
+
+    if (slideIndexRev > slidesRev.length) {
+        slideIndexRev = 1;
+    } else if (slideIndexRev < 1) {
+        slideIndexRev = slidesRev.length;
+    }
+
+    for (let i = 0; i < slidesRev.length; i++) {
+        slidesRev[i].style.display = 'none';
+    }
+
+    slidesRev[slideIndexRev - 1].style.display = 'block';
+}
+
+showSlidesRev();
+
+
+
+
+
+/* Levantamiento */
+
+let slideIndexLevan = 0;
+
+function showSlidesLevan() {
+    const slidesLevan = document.getElementsByClassName('slideLevan');
+
+    for (let i = 0; i < slidesLevan.length; i++) {
+        slidesLevan[i].style.display = 'none';
+    }
+
+    slideIndexLevan++;
+
+    if (slideIndexLevan > slidesLevan.length) {
+        slideIndexLevan = 1;
+    }
+
+    slidesLevan[slideIndexLevan - 1].style.display = 'block';
+
+    setTimeout(showSlidesLevan, 6000);
+}
+
+function changeSlideLevan(n) {
+    slideIndexLevan += n;
+    const slidesLevan = document.getElementsByClassName('slideLevan');
+
+    if (slideIndexLevan > slidesLevan.length) {
+        slideIndexLevan = 1;
+    } else if (slideIndexLevan < 1) {
+        slideIndexLevan = slidesLevan.length;
+    }
+
+    for (let i = 0; i < slidesLevan.length; i++) {
+        slidesLevan[i].style.display = 'none';
+    }
+
+    slidesLevan[slideIndexLevan - 1].style.display = 'block';
+}
+
+showSlidesLevan();
+
+
+
+
+
+/* Analisis */
+
+let slideIndexAnalis = 0;
+
+function showSlidesAnalis() {
+    const slidesAnalis = document.getElementsByClassName('slideAnalis');
+
+    for (let i = 0; i < slidesAnalis.length; i++) {
+        slidesAnalis[i].style.display = 'none';
+    }
+
+    slideIndexAnalis++;
+
+    if (slideIndexAnalis > slidesAnalis.length) {
+        slideIndexAnalis = 1;
+    }
+
+    slidesAnalis[slideIndexAnalis - 1].style.display = 'block';
+
+    setTimeout(showSlidesAnalis, 6000);
+}
+
+function changeSlideAnalis(n) {
+    slideIndexAnalis += n;
+    const slidesAnalis = document.getElementsByClassName('slideAnalis');
+
+    if (slideIndexAnalis > slidesAnalis.length) {
+        slideIndexAnalis = 1;
+    } else if (slideIndexAnalis < 1) {
+        slideIndexAnalis = slidesAnalis.length;
+    }
+
+    for (let i = 0; i < slidesAnalis.length; i++) {
+        slidesAnalis[i].style.display = 'none';
+    }
+
+    slidesAnalis[slideIndexAnalis - 1].style.display = 'block';
+}
+
+showSlidesAnalis();
