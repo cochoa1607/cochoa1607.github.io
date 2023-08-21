81585d1d2d91c31d54c15aafa6836238051efa68
Carlos Ochoa <115586395+cochoa1607@users.noreply.github.com>
2023-08-21 17:51:39 -0600

    ROD

--- dev/rod/js/collapseExperiencia.js
+++ dev/rod/js/collapseExperiencia.js
@@ -15,3 +15,90 @@ var contTexto_002 = document.querySelector(".contenedor-texto-002");
 function masInfoAer002() {
     contTexto_002.classList.toggle("active-contenedor-texto-002");
 }
+
+/* Aeropuerto_003 */
+document.querySelector(".aeropuerto_003_masInfo").addEventListener("click", masInfoAer003);
+
+var contTexto_003 = document.querySelector(".contenedor-texto-003");
+
+function masInfoAer003() {
+    contTexto_003.classList.toggle("active-contenedor-texto-003");
+}
+
+/* Aeropuerto_004 */
+document.querySelector(".aeropuerto_004_masInfo").addEventListener("click", masInfoAer004);
+
+var contTexto_004 = document.querySelector(".contenedor-texto-004");
+
+function masInfoAer004() {
+    contTexto_004.classList.toggle("active-contenedor-texto-004");
+}
+
+/* Aeropuerto_005 */
+document.querySelector(".aeropuerto_005_masInfo").addEventListener("click", masInfoAer005);
+
+var contTexto_005 = document.querySelector(".contenedor-texto-005");
+
+function masInfoAer005() {
+    contTexto_005.classList.toggle("active-contenedor-texto-005");
+}
+
+
+/* Aeropuerto_006 */
+document.querySelector(".aeropuerto_006_masInfo").addEventListener("click", masInfoAer006);
+
+var contTexto_006 = document.querySelector(".contenedor-texto-006");
+
+function masInfoAer006() {
+    contTexto_006.classList.toggle("active-contenedor-texto-006");
+}
+
+
+/* Aeropuerto_007 */
+document.querySelector(".aeropuerto_007_masInfo").addEventListener("click", masInfoAer007);
+
+var contTexto_007 = document.querySelector(".contenedor-texto-007");
+
+function masInfoAer007() {
+    contTexto_007.classList.toggle("active-contenedor-texto-007");
+}
+
+
+/* Aeropuerto_008 */
+document.querySelector(".aeropuerto_008_masInfo").addEventListener("click", masInfoAer008);
+
+var contTexto_008 = document.querySelector(".contenedor-texto-008");
+
+function masInfoAer008() {
+    contTexto_008.classList.toggle("active-contenedor-texto-008");
+}
+
+
+/* Aeropuerto_009 */
+document.querySelector(".aeropuerto_009_masInfo").addEventListener("click", masInfoAer009);
+
+var contTexto_009 = document.querySelector(".contenedor-texto-009");
+
+function masInfoAer009() {
+    contTexto_009.classList.toggle("active-contenedor-texto-009");
+}
+
+
+/* Aeropuerto_010 */
+document.querySelector(".aeropuerto_010_masInfo").addEventListener("click", masInfoAer010);
+
+var contTexto_010 = document.querySelector(".contenedor-texto-010");
+
+function masInfoAer010() {
+    contTexto_010.classList.toggle("active-contenedor-texto-010");
+}
+
+
+/* Seguridad_001 */
+document.querySelector(".seguridad_001_masInfo").addEventListener("click", masInfoSeg001);
+
+var contTextoSeg_001 = document.querySelector(".contenedor-texto-seg-001");
+
+function masInfoSeg001() {
+    contTextoSeg_001.classList.toggle("active-contenedor-texto-seg-001");
+}
