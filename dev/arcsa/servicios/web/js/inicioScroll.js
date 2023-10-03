abe47c18d40eb1fcf76ed39a7732212afac2cec5
Carlos Ochoa <115586395+cochoa1607@users.noreply.github.com>
2023-10-03 14:57:18 -0600

    A

--- dev/arcsa/servicios/web/js/inicioScroll.js
+++ dev/arcsa/servicios/web/js/inicioScroll.js
@@ -1,15 +1,15 @@
 window.addEventListener("scroll", function () {
-    var menu = document.querySelector(".containerHeader");
+    var menu = document.querySelector(".contenedorBanner");
     menu.classList.toggle("abajo", window.scrollY > 120);
 });
 
 window.addEventListener("scroll", function () {
-    var vid = document.querySelector(".containerVideo");
+    var vid = document.querySelector(".vid");
     vid.classList.toggle("abajo", window.scrollY > 120);
 });
 
 window.addEventListener("scroll", function () {
-    var menuBGText = document.querySelector(".containerServicios");
+    var menuBGText = document.querySelector(".texto");
     menuBGText.classList.toggle("abajo", window.scrollY > 80);
 });
 
