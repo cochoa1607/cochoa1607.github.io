97a93c83389ffd762772ec9d6c1cd8c375f83b81
Carlos Ochoa <115586395+cochoa1607@users.noreply.github.com>
2024-01-03 13:31:26 -0600

    A

--- dev/arcsa/capital/WebApp/webapp_arcsacapital/src/app/app.module.ts
+++ dev/arcsa/capital/WebApp/webapp_arcsacapital/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 
@@ -10,7 +11,8 @@ import { ContadorComponent } from './contador/contador.component';
     ContadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
