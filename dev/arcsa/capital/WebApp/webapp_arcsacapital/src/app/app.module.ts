8ece32bbcb120151b4b3cefba450db97bfa53b4d
Carlos Ochoa <115586395+cochoa1607@users.noreply.github.com>
2024-01-03 13:18:04 -0600

    A

--- dev/arcsa/capital/WebApp/webapp_arcsacapital/src/app/app.module.ts
+++ dev/arcsa/capital/WebApp/webapp_arcsacapital/src/app/app.module.ts
@@ -1,6 +1,4 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 
@@ -10,7 +8,8 @@ import { ContadorComponent } from './contador/contador.component';
     ContadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- dev/arcsa/capital/WebApp/webapp_arcsacapital/src/app/contador/contador.component.ts
+++ dev/arcsa/capital/WebApp/webapp_arcsacapital/src/app/contador/contador.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-contador',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./contador.component.css']
 })
 export class ContadorComponent {
+
+
+  persona: Persona = {
+    nombre: 'Daniel Ochoa',
+    edad: 26
+  };
 
 }
