40de47a9be360ecc7ac1ef1788a3a9bb94f29574
Carlos Daniel Ochoa <carlos.ochoa1607@gmail.com>
2024-11-22 12:01:09 -0600

    Sube cambios pasados

---
+++ dev/platzi/js/js-basico/operadores.js
@@ -0,0 +1,40 @@
+// Operadores de comparacion
+
+// == igualdad en valor
+
+// === igualdad en valor y tipo de dato
+
+// != diferente de valor
+
+// !== diferente de valor y dato
+
+// > mayor a otro valor
+
+// < menor a otro valor
+
+// >= mayor o igual a otro valor
+
+// <= menor o igual a otro valor
+
+const a = 10
+const b = 20
+const c = "10"
+
+a == b
+
+    // Operadores logicos
+
+    &&
+
+||
+
+    !
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+
+// Estructuras de control
+
+if (let === algo)
